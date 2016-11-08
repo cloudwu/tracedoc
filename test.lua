242f229a91fd43f06cc498e587699c1dc539b634
Cloud Wu <cloudwu@gmail.com>
2016-11-08 12:00:11 +0800

    hello world

---
+++ test.lua
@@ -0,0 +1,56 @@
+local tracedoc = require "tracedoc"
+
+local doc = tracedoc.new {
+	a = 1,
+	b = { 1,2,3 },
+	c = { d = 4 , e = 5 },
+}
+
+local function dump(doc)
+	local changes = tracedoc.commit(doc, {})
+	print("Dump:")
+	for k,v in pairs(doc) do
+		print(k,v)
+		if type(v) == "table" then
+			for k, v in pairs(v) do
+				print("\t",k,v)
+			end
+		end
+	end
+	print("changes:")
+	for k,v in pairs(changes) do
+		print(k,v)
+	end
+end
+
+dump(doc)
+
+doc.a = nil	-- change
+
+assert(doc.a == nil)
+assert(doc.b[1] == 1)
+assert(doc.b[2] == 2)
+assert(doc.b[3] == 3)
+assert(doc.c.d == 4)
+
+doc.b[1] = 0	-- change
+doc.b[2] = 2	-- not change
+doc.b[3] = nil	-- remove
+
+local tmp = doc.c
+
+dump(doc)
+
+doc.a = 2	-- change
+assert(doc.a == 2)
+doc.b = 2	-- change and delete table
+assert(doc.b == 2)
+doc.c = { e = 5 } -- change table
+assert(doc.c.d == nil)
+doc.b = nil
+
+dump(doc)
+
+assert(tmp.e == 5)
+
+
---
+++ tracedoc.lua
@@ -0,0 +1,133 @@
+local next = next
+local pairs = pairs
+local setmetatable = setmetatable
+local type = type
+local rawset = rawset
+
+local tracedoc = {}
+
+local function doc_pairs(doc)
+	return next, doc._lastversion
+end
+
+local function doc_len(doc)
+	return #doc._lastversion
+end
+
+local function doc_change(t, k, v)
+	rawset(t,k,v)
+	if v == nil then
+		if t._keys[k] then	-- already set
+			t._doc._lastversion[k] = nil
+		elseif t._doc._lastversion[k] == nil then	-- ignore since lastversion is nil
+			return
+		end
+		t._doc._lastversion[k] = nil
+	end
+	t._keys[k] = true
+end
+
+tracedoc.null = setmetatable({} , { __tostring = function() return "NULL" end })	-- nil
+
+function tracedoc.new(init)
+	local doc = {
+		_changes = { _keys = {} },
+		_lastversion = {},
+	}
+	doc._changes._doc = doc
+	setmetatable(doc._changes, {
+		__index = doc._lastversion,
+		__newindex = doc_change,
+	})
+	setmetatable(doc, {
+		__newindex = doc._changes,
+		__index = doc._changes,
+		__pairs = doc_pairs,
+		__len = doc_len,
+	})
+	if init then
+		for k,v in pairs(init) do
+			doc[k] = v
+		end
+		tracedoc.commit(doc)
+	end
+	return doc
+end
+
+local function doc_copy(doc, k, sub_doc, result, prefix)
+	local target_doc = doc[k]
+	if type(target_doc) ~= "table" then
+		target_doc = tracedoc.new()
+		doc[k] = target_doc
+		for k,v in pairs(sub_doc) do
+			target_doc[k] = v
+			if result then
+				local key = prefix and prefix .. k or k
+				result[key] = v
+			end
+		end
+	else
+		for k in pairs(target_doc) do
+			if sub_doc[k] == nil then
+				target_doc[k] = nil
+				if result then
+					local key = prefix and prefix .. k or k
+					result[key] = tracedoc.null
+				end
+			end
+		end
+		for k,v in pairs(sub_doc) do
+			if target_doc[k] ~= v then
+				target_doc[k] = v
+				if result then
+					local key = prefix and prefix .. k or k
+					result[key] = v
+				end
+			end
+		end
+	end
+	return target_doc
+end
+
+function tracedoc.commit(doc, result, prefix)
+	local lastversion = doc._lastversion
+	local changes = doc._changes
+	local keys = changes._keys
+	for k in pairs(keys) do
+		local v = changes[k]
+		keys[k] = nil
+		if type(v) == "table" then
+			if result then
+				local key = (prefix and prefix .. k or k) .. "."
+				v = doc_copy(lastversion, k, v, result, key)
+			else
+				v = doc_copy(lastversion, k, v)
+			end
+		elseif v == nil then
+			if result then
+				local key = prefix and prefix .. k or k
+				result[key] = tracedoc.null
+			end
+		elseif lastversion[k] ~= v then
+			lastversion[k] = v
+			if result then
+				local key = prefix and prefix .. k or k
+				result[key] = v
+			end
+		end
+		doc._changes[k] = nil
+	end
+	for k,v in pairs(lastversion) do
+		if type(v) == "table" then
+			if result then
+				local key = (prefix and prefix .. k or k) .. "."
+				tracedoc.commit(v, result, key)
+			else
+				tracedoc.commit(v)
+			end
+		end
+	end
+	return result
+end
+
+return tracedoc
