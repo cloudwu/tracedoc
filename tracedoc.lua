68d0632d17dd33f6a73beafc5a507f590e95412e
jojo59516 <ycbuaa2011@buaa.edu.cn>
2018-12-29 15:21:01 +0800

    fix bugs leading to tracedoc.next() skipping 'false' values

--- tracedoc.lua
+++ tracedoc.lua
@@ -22,7 +22,7 @@ local function doc_next(doc, key)
 				break
 			end
 			local v = doc[key]
-			if v then
+			if v ~= nil then
 				return key, v
 			end
 		end
@@ -39,7 +39,7 @@ local function doc_next(doc, key)
 		end
 		if not change_keys[key] then
 			local v = doc[key]
-			if v then
+			if v ~= nil then
 				return key, v
 			end
 		end
